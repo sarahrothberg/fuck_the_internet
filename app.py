aaff77803bf5ea34c80e7af2fafc0b5e65631295
Sarah Rothberg <sarahrothberg@gmail.com>
2014-02-25 12:13:29 -0500

    changed some stuff

--- app.py
+++ app.py
@@ -13,23 +13,23 @@ app = Flask(__name__)
 account_sid = "AC2f3087700e52df09a6bf868cd422d398"
 auth_token  = "fe3eecfae2e9747c93c67dce12458853"
 client = TwilioRestClient(account_sid, auth_token)
- 
 
-messages = client.messages.list(
+message = client.messages.list(
 	 to= "+17472334999"
 )
 
 @app.route("/", methods=['GET', 'POST'])
-def displayQuestion():
-    return messages[0].body
 
-# @app.route("/response", methods=['GET', 'POST'])
-# def fromTheInternet():
-#     resp = twilio.twiml.Response()
-#     resp.message("THANKS FOR UR QUESTION. <3, THE INTERNET")
-#     return str(resp)
+def displayQuestion():
+    return message[0].body
+
+@app.route("/response", methods=['GET', 'POST'])
+def fromTheInternet():
+    resp = twilio.twiml.Response()
+    resp.message("THANKS FOR UR QUESTION. <3, THE INTERNET")
+    return str(resp)
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host = '0.0.0.0', port=port)
-	# app.run(debug=True)
+	app.run(debug=True)
