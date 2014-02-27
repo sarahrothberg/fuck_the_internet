e4d27b6ab03f383f21d35fd346372138eebd5c33
Sarah Rothberg <sarahrothberg@gmail.com>
2014-02-27 12:52:53 -0500

    First Push
    This code responds to incoming SMSs and also posts the last message but
    doesn’t update with incoming messages.
    
    In order to use it, you may need to pip install twilio and/or twimil.
    
    It exists on a heroku server at popupinternet.herokuapp.com.

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 # Download the Python helper library from twilio.com/docs/python/install
 from twilio.rest import TwilioRestClient
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 from twilio.util import TwilioCapability
 
 import twilio.twiml
 import os
 
+
+SECRET_KEY = 'a secret key'
 app = Flask(__name__)
 
 
 # Your Account Sid and Auth Token from twilio.com/user/account
+
 account_sid = "AC2f3087700e52df09a6bf868cd422d398"
 auth_token  = "fe3eecfae2e9747c93c67dce12458853"
 client = TwilioRestClient(account_sid, auth_token)
@@ -19,14 +22,15 @@ message = client.messages.list(
 )
 
 @app.route("/", methods=['GET', 'POST'])
-
 def displayQuestion():
+	resp = twilio.twiml.Response()
     return message[0].body
 
 @app.route("/response", methods=['GET', 'POST'])
 def fromTheInternet():
+	"""Respond to incoming calls with a simple text message."""
     resp = twilio.twiml.Response()
-    resp.message("THANKS FOR UR QUESTION. <3, THE INTERNET")
+    resp.message("THANKS FOR UR QUESTION. FROM, THE INTERNET")
     return str(resp)
 
 if __name__ == "__main__":
