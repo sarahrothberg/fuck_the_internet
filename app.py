226736a26fd0228eb43720f2ceaaf7e344799ed2
Sarah Rothberg <sarahrothberg@gmail.com>
2014-03-01 21:08:29 -0500

    adding stuff
    hi val and mack

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # Download the Python helper library from twilio.com/docs/python/install
 from twilio.rest import TwilioRestClient
-from flask import Flask, request, redirect, session
 from flask import Flask, request, redirect, session 
 from flask import render_template
 from twilio.util import TwilioCapability
@@ -17,9 +16,6 @@ app = Flask(__name__)
 account_sid = "AC2f3087700e52df09a6bf868cd422d398"
 auth_token  = "fe3eecfae2e9747c93c67dce12458853"
 client = TwilioRestClient(account_sid, auth_token)
-message = client.messages.list(
-	 to= "+17472334999"
-)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -30,12 +26,12 @@ def displayQuestion():
 	smsMessage = message[0].body
 	return render_template ('main.html', smsMessage = smsMessage)
 
-@app.route("/textRefresher", methods=['GET', 'POST'])
-def textReferesher():
-	message = client.messages.list(
-		to = "+17472334999"
-		)
-	return message[0].body
+# @app.route("/textRefresher", methods=['GET', 'POST'])
+# def textReferesher():
+# 	message = client.messages.list(
+# 		to = "+17472334999"
+# 		)
+# 	return message[0].body
 
 # @app.route("/", methods=['GET', 'POST'])
 # def storesMessages():
@@ -48,13 +44,11 @@ def textReferesher():
 @app.route("/response", methods=['GET', 'POST'])
 def fromTheInternet():
 	"""Respond to incoming calls with a simple text message."""
-	
 	resp = twilio.twiml.Response()
 	resp.message("THANKS FOR UR QUESTION. FROM, THE INTERNET")
 	return str(resp)
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 5000))
 	port = int(os.environ.get('PORT', 8090))
 	app.run(host = '0.0.0.0', port=port)
 	app.run(debug=True)
