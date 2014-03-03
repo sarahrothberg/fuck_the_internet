45ed3159e14d35b55b07f7c67d9a0ed9aa89dcd7
Sarah Rothberg <sarahrothberg@gmail.com>
2014-03-02 19:06:23 -0500

    testing array

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def fromTheInternet():
 	for word in badwords:
 		if word in textWords:
 			msg = "OH NO A$$FACE SAFESEARCH IS ON!"
-	else:
-		msg = "THANKS FOR UR QUESTION. FROM, THE INTERNET"
+		else:
+			msg = "THANKS FOR UR QUESTION. FROM, THE INTERNET"
 	resp = twilio.twiml.Response()
 	resp.message(msg)
 	return str(resp)
